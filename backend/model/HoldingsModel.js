2bd180fe97691246598c1d3ec11c76ca61f10cc2
aastha-1511 <158474938+aastha-1511@users.noreply.github.com>
2025-08-30 01:08:32 +0530

    Update HoldingsModel.js

--- backend/model/HoldingsModel.js
+++ backend/model/HoldingsModel.js
@@ -1,7 +1,7 @@
 const { model } = require("mongoose");
 
-const { HoldingsSchema } = require("../schemas/HoldingsSchema");
+const { HoldingSchema } = require("../schemas/HoldingsSchema");
 
-const HoldingsModel = new model("holding", HoldingsSchema);
+const HoldingsModel = new model("holding", HoldingSchema);
 
 module.exports = { HoldingsModel };
