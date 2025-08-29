3276e7e19423c1fc33398103dbb9f20f50092918
aastha-1511 <158474938+aastha-1511@users.noreply.github.com>
2025-08-30 01:07:39 +0530

    Update OrdersSchema.js

--- backend/schemas/OrdersSchema.js
+++ backend/schemas/OrdersSchema.js
@@ -1,10 +1,10 @@
 const { Schema } = require("mongoose");
 
-const OrdersSchema = new Schema({
+const OrderSchema = new Schema({
   name: String,
   qty: Number,
   price: Number,
   mode: String,
 });
 
-module.exports = { OrdersSchema };
+module.exports = { OrderSchema };
